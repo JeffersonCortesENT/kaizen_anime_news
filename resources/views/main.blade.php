9aa8cc9d41b6438a02730d7c378b5101a1191c53
JeffersonCortesENT <jeffersoncortesenterprise@gmail.com>
2023-08-03 14:21:31 +0800

    Added configuration for compiled files in production

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,11 +4,19 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <title>Kaizen News Network</title>
     <meta charset="utf-8" />
-    @viteReactRefresh
-    @vite('resources/js/index.jsx')
-    @vite('resources/css/app.css')
+    @env('local')
+      @viteReactRefresh
+      @vite('resources/js/index.jsx')
+      @vite('resources/css/app.css')
+    @endenv
+    @env('production')
+      <link rel="stylesheet" src="{{ env('PRODUCTION_URL') }}/build/assets/app.css"></link>
+    @endenv
   </head>
   <body>
     <div id="root"></div>
+    @env('production')
+      <script src="{{ env('PRODUCTION_URL') }}/build/assets/index.js"></script>
+    @endenv
   </body>
 </html>
