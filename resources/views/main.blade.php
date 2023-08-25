3e15a1e1a258756fbb3bef582a4852d1ae7ac142
JeffersonCortesENT <jeffersoncortesenterprise@gmail.com>
2023-08-25 14:28:32 +0800

    Added website icon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,8 @@
     <title>Kaizen News Network</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/logo/kaizen_icon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/logo/kaizen_icon.png') }}" type="image/x-icon">
     @env('local')
       @viteReactRefresh
       @vite('resources/js/index.jsx')
